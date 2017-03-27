83bb6b8f6562eea0959ec5daf11ae2d4bc31c6d5
Daniel Chavez <bitclaw@gmail.com>
2017-03-27 15:12:53 -0500

    apptmnt: Simplify project template since we won't be using bootstrap, jQuery ..etc.

---
+++ app/Traits/Apptmnt.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Traits;
+
+use Exception;
+
+/**
+ * Trait Apptmnt
+ */
+trait Apptmnt
+{
+
+    public function create()
+    {
+    }
+}
---
+++ resources/views/apptmnt.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Apptmnt</title>
+    <!-- Apptmnt Styles -->
+    <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
+    <!-- Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,500,700,300,600,400italic,300italic,700italic,800,900' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <h1>Welcome to the Appointment Creator!</h1>
+    </div>
+</div>
+    <!-- Scripts -->
+    <script src="{{ elixir('js/app.js') }}"></script>
+</body>
+</html>
