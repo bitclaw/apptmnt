b0f614560c263dd4606bb3cb663c8bcba8b14eab
Daniel C <bitclaw@gmail.com>
2017-04-09 18:51:49 -0500

    Add appointment module

--- src/apptmnt/apptmnt.js
+++ src/apptmnt/apptmnt.js
@@ -1,8 +1,25 @@
-export default class Apptmnt {
+export default class Apptmnt extends HTMLElement {
+    static get selector () { return 'apptmnt-creator' }
+
     constructor() {
+        super();
         this._name = 'Apptmnt';
     }
+
     get name() {
         return this._name;
+    }
+
+    render () {
+        const innerHTML = template({
+            date: this.date
+        });
+
+        this.element.innerHTML = `<style>${style}</style>${innerHTML}`;
+        this.addEventListeners()
+    }
+
+    attachEventListeners () {
+        this.element.querySelector('app-datepicker').addEventListener('selected-date', ({ detail }) => this.updateDate(detail));
     }
 }
