b6f8542375714b11d29184861b72644c96351c65
Daniel C <bitclaw@gmail.com>
2017-04-09 18:49:35 -0500

    Add components to entry file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-// Test
+import { Datepicker } from './datepicker/datepicker'
+import { Apptmnt } from './apptmnt/apptmnt'
+
+const components = [
+    Datepicker,
+    Apptmnt
+];
+
+components.forEach(component => customElements.define(component.selector, component));
